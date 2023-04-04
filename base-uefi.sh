4454f8971567d2e748663253ff76d782ee2f3f45
Manoel Saraiva <manoeljsaraiva@gmail.com>
2023-04-03 23:23:22 -0300

    Alteracoes

--- base-uefi.sh
+++ base-uefi.sh
@@ -17,12 +17,12 @@ echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
 echo $layout_teclado >> /etc/vconsole.conf
 
 # Adicionar um nome ao computador
-echo "pinoquio" >> /etc/hostname
+echo $nome_pc >> /etc/hostname
 
 # Configuração do arquivo hosts
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 pinoquio.localdomain pinoquio" >> /etc/hosts
+echo "127.0.1.1 $nome_pc.localdomain $nome_pc" >> /etc/hosts
 
 # Senha do usuario root
 echo root:password | chpasswd
@@ -30,13 +30,13 @@ echo root:password | chpasswd
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
 
 # pacman -S --noconfirm xf86-video-amdgpu
 # pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
 # Instalar o grub
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
 
 
 # Gerar o grub.cfg
